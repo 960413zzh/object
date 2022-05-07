15c27662ad8bed464966382426b833800503a084
960413zzh <95685578+960413zzh@users.noreply.github.com>
2022-05-07 15:46:35 +0800

    Update train_SRC_MT.py

--- code/train_SRC_MT.py
+++ code/train_SRC_MT.py
@@ -29,9 +29,9 @@ from validation import epochVal, epochVal_metrics
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_path', type=str, default="../input/cifar-10-data", help='dataset root dir')
-parser.add_argument('--csv_file_train', type=str, default="D:/Dataset_01/cifar10_training.csv", help='training set csv file')
+parser.add_argument('--csv_file_train', type=str, default="./source_code_of_cifar10/data/cifar_10/cifar10_training.csv", help='training set csv file')
 # parser.add_argument('--csv_file_val', type=str, default='C:/Users/30812/Desktop/Test code of skin/SRC-MT-master/data/skin/validation.csv', help='validation set csv file')
-parser.add_argument('--csv_file_test', type=str, default="D:/Dataset_01/cifar10_testing.csv", help='testing set csv file')
+parser.add_argument('--csv_file_test', type=str, default="./source_code_of_cifar10/data/cifar_10/cifar10_testing.csv", help='testing set csv file')
 parser.add_argument('--exp', type=str,  default='xxxx', help='model_name')
 parser.add_argument('--epochs', type=int,  default=100, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int, default=16, help='batch_size per gpu')
